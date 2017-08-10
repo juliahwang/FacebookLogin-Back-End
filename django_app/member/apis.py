98ca746008ebb64a0b6e572e4f888934fa6dae26
JuliaHwang <qufskan9396@gmail.com>
2017-08-10 17:10:58 +0900

    add UserManager to specify user_type

--- django_app/member/apis.py
+++ django_app/member/apis.py
@@ -1,4 +1,6 @@
 import requests
+from django.contrib.auth import get_user_model
+from rest_framework.authtoken.models import Token
 from rest_framework.exceptions import APIException
 from rest_framework.response import Response
 from rest_framework.views import APIView
@@ -8,6 +10,8 @@ from config import settings
 __all__ = (
     'FacebookLoginAPIView',
 )
+
+User = get_user_model()
 
 
 class FacebookLoginAPIView(APIView):
@@ -25,8 +29,24 @@ class FacebookLoginAPIView(APIView):
 
         # 프론트로부터 전달받은 token을 Facebook의 debug_token API를 이용해
         # 검증한 결과를 debug_result에 할당
-        debug_result = self.debug_token(token)
-        return Response(debug_result)
+        self.debug_token(token)
+        user_info = self.get_user_info(token=token)
+
+        # 이미 존재하면 가져오고 없으면 페이스북 유저 생성
+        if User.objects.filter(username=user_info['id']).exists():
+            user = User.objects.get(username=user_info['id'])
+        else:
+            user = User.objects.create_facebook_user(user_info)
+
+        # DRF 토큰을 생성
+        token, token_created = Token.objects.get_or_create(user=user)
+
+        # 관련정보를 한번에 리턴
+        ret = {
+            'token': token.key,
+            # 'user': UserSerializer(user).data,
+        }
+        return Response(ret)
 
     def debug_token(self, token):
         """
@@ -45,3 +65,22 @@ class FacebookLoginAPIView(APIView):
             raise APIException('token invalid')
         else:
             return result
+
+    def get_user_info(user_id, token):
+        url_user_info = 'https://graph.facebook.com/v2.9/me'
+        url_user_info_params = {
+            'access_token': token,
+            # 권한을 요청하지 않아도 오는 기본 정보
+            # 반드시 scope 내용을 적어줘야한다.
+            'fields': ','.join([
+                'id',
+                'name',
+                'first_name',
+                'last_name',
+                'picture.type(large)',
+                'gender',
+            ])
+        }
+        response = requests.get(url_user_info, params=url_user_info_params)
+        result = response.json()
+        return result
--- django_app/member/models.py
+++ django_app/member/models.py
@@ -1,8 +1,24 @@
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import AbstractUser, UserManager as DjangoUserManager
 from django.db import models
 
 # Create your models here.
 
 
+class UserManager(DjangoUserManager):
+    def create_facebook_user(self, user_info):
+        return self.create_user(
+            username=user_info['id'],
+            first_name=user_info['first_name', ''],
+            last_name=user_info['last_name', ''],
+            user_types=User.USER_TYPE_FACEBOOK
+        )
+
+
 class User(AbstractUser):
+    USER_TYPE_DJANGO = 'django'
+    USER_TYPE_FACEBOOK = 'facebook'
+    CHOICES_USER_TYPE = (
+        (USER_TYPE_DJANGO, 'Django'),
+        (USER_TYPE_FACEBOOK, 'Facebook'),
+    )
     pass
